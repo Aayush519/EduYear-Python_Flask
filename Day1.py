6f5efcfafbfe497da869e8e6603460698c4f3a0e
Aayush519 <31105089+Aayush519@users.noreply.github.com>
2021-04-24 18:06:14 +0530

    Delete Day1.py

--- Day1.py
+++
@@ -1,9 +0,0 @@
-companyName = "Tesla"
-yearFounded = 2003
-companyStock = 729.40
-fortune500Company = True
-
-print("{} is electric car manufacturer.".format(companyName))
-print("{} is founded in year {}".format(companyName,yearFounded))
-print("{} has a stock price of {}".format(companyName,companyStock),end=' USD\n')
-print("Is {} Fortune 500 Company? - {}".format(companyName,fortune500Company))
